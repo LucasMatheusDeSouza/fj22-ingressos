d2140957e197493239cdc4ea806814df61aa116e
Lucas Matheus De Souza Carvalho <lucas.matheus@yahoo.com.br>
2019-01-28 17:18:21 -0200

    adicionando

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,12 +2,30 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
 	
-	private Integer id;
+	@Id
+	@GeneratedValue
+	private Long id;
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	
+	@ManyToOne
 	private Filme filme;
+	
+	/**
+	 * 
+	 * @deprecated hibernate only
+	 */
+	public Sessao() {}
 	
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
@@ -17,29 +35,15 @@ public class Sessao {
 	public LocalTime getHorarioTermino() {
 		return this.horario.plusMinutes(filme.getDuracao().toMinutes());
 	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
 	public LocalTime getHorario() {
 		return horario;
-	}
-	public void setHorario(LocalTime horario) {
-		this.horario = horario;
 	}
 	public Sala getSala() {
 		return sala;
 	}
-	public void setSala(Sala sala) {
-		this.sala = sala;
-	}
 	public Filme getFilme() {
 		return filme;
 	}
-	public void setFilme(Filme filme) {
-		this.filme = filme;
-	}
+	
 
 }
