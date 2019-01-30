5e7037376036a97a27acf4191cc11b4713340ec2
Lucas Matheus <lucas.matheus@yahoo.com.br>
2019-01-30 16:15:41 -0200

    adicionando detalhes do filme

--- src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
+++ src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
@@ -18,8 +18,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.DetalhesDoFilme;
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.rest.OmdbClient;
 
 /**
  * Created by nando on 03/03/17.
@@ -33,6 +35,9 @@ public class FilmeController {
     
     @Autowired
     private SessaoDao sessaoDao;
+    
+    @Autowired
+    private OmdbClient client;
 
     @GetMapping({"/admin/filme", "/admin/filme/{id}"})
     public ModelAndView form(@PathVariable("id") Optional<Integer> id, Filme filme){
@@ -48,11 +53,14 @@ public class FilmeController {
         return modelAndView;
     }
 
-	@GetMapping("/filme/em-cataz")
+	@GetMapping("/filme/em-cartaz")
     public ModelAndView emcartaz() {
-    	ModelAndView modelAndView = new ModelAndView("filme/em-cartaz");
-    	modelAndView.addObject("filmes",filmeDao.findAll());
-    	return modelAndView;
+    	
+		ModelAndView modelAndView = new ModelAndView("filme/em-cartaz");
+    	
+		modelAndView.addObject("filmes",filmeDao.findAll());
+    	
+		return modelAndView;
     }
 
 
@@ -96,7 +104,12 @@ public class FilmeController {
         ModelAndView modelAndView = new ModelAndView("filme/detalhe");
         Filme filme = filmeDao.findOne(id);
         List<Sessao> sessoes = sessaoDao.buscaSessoesDoFilme(filme);
+       
+        Optional<DetalhesDoFilme> detalhesDoFilme = client.request(filme);
+        
+        
         modelAndView.addObject("sessoes",sessoes);
+        modelAndView.addObject("detalhes",detalhesDoFilme.orElse(new DetalhesDoFilme()));
 
         return modelAndView;
     }
